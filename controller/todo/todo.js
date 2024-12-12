2abc1706f795b42bfb32eda925dedeac5c23bba4
hey-yoon <yooniyunisss@gmail.com>
2024-12-13 01:27:31 +0900

    todo- insert로직 구현완료

--- controller/todo/todo.js
+++ controller/todo/todo.js
@@ -2,14 +2,27 @@ import Todo from "../../models/todo_schema.js"
 
 // 추가 하는 로직(POST)
 const insertTodo = async(req,res)=>{
-    console.log(req)
-    const {id,title,isChecked} = req.body;
-    await Todo.create(title);
+    try{
+        const {id,title,isChecked} = req.body;
+        const add = await Todo.create({ id, title, isChecked });
+        res.json(add);
+    } catch (error){
+        res.status(500).json({ error: "데이터베이스 오류" });
+    }
 }
 
 // 체크 상태 관리(PUT)
 const checkTodo = async(req,res)=>{
-
+    // try{
+    //     const findId = await Todo.findOne({isChecked: req.body.isChecked})
+    //     const updateCheck = await Todo.updateOne(findId,{
+    //         isChecked: !isChecked
+    //     })
+    //     res.json(updateCheck)
+    // }
+    // catch(error){
+    //     res.status(500).json({error:"데이터베이스 오류"});
+    // }
 }
 
 // 리스트 삭제(DELETE)
