7cebf0f0a717f61a54efc113142195913cc2550a
hey-yoon <yooniyunisss@gmail.com>
2024-12-16 21:40:34 +0900

    todo수정

--- controller/todo/todo.js
+++ controller/todo/todo.js
@@ -12,38 +12,15 @@ const insertTodo = async(req,res)=>{
 }
 
 // 체크 상태 관리(PUT)
-// const checkTodo = async(req,res)=>{
-    // try{
-    //     const findId = await Todo.findOne({isChecked: req.body.isChecked})
-    //     const updateCheck = await Todo.updateOne(findId,{
-    //         isChecked: !isChecked
-    //     })
-    //     res.json(updateCheck)
-    // }
-    // catch(error){
-    //     res.status(500).json({error:"데이터베이스 오류"});
-    // }
-// }
 
 const checkTodo = async (req, res) => {
     try {
         console.log(req)
-        const { id, isChecked } = req.body; // 클라이언트에서 전송된 데이터 구조 분해
+        const { id, isChecked } = req.body;
         
-        if (!id) {
-            return res.status(400).json({ error: "ID가 없습니다." });
-        }
-
-        // 해당 ID의 항목 찾기
-        const findTodo = await Todo.findOne({ id });
-        if (!findTodo) {
-            return res.status(404).json({ error: "해당 ID를 가진 Todo를 찾을 수 없습니다." });
-        }
-
-        // isChecked 상태 업데이트
         const updateCheck = await Todo.updateOne(
-            { id }, // 조건
-            { $set: { isChecked: !isChecked } } // 업데이트할 내용
+            { id },
+            { isChecked: !isChecked}
         );
 
         res.status(200).json({ message: "성공적으로 업데이트되었습니다.", updateCheck });
