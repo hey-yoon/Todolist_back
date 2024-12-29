f5bca144f687d0c5036379e869e517975d9df461
hey-yoon <yooniyunisss@gmail.com>
2024-12-29 23:03:41 +0900

    user_schema 생성

--- models/user_schema.js
+++ models/user_schema.js
@@ -1,13 +1,16 @@
 import mongoose, {Schema, model} from "mongoose";
+import { getCurrentTime } from "../utils/utils";
 
 const userSchema = new Schema({
-    email:{},
-    password:{},
-    name:{},
-    age:{},
-    phone:{},
-    address:{},
-    token:{}
+    email:{type:String, require:true, unique:true},
+    password:{type:String, require:true},
+    name:String,
+    age:{type:Number, default:0},
+    phone:{type:String,default:"000-0000-0000"},
+    address:String,
+    token:String, //refresh토큰
+    createAt: {type:String,default: getCurrentTime},
+    updateAt: {type:String,default: getCurrentTime}
 })
 
 export default model("User", userSchema, "users"); 
